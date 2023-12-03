9366ae2c11943ef4ff20560758e664bfd9eccfd2
Yash Tapre <yashtapre77@gmail.com>
2023-12-03 23:30:32 +0530

    routes learn

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return "Hello From RCOEM";
 });
+Route::get('/aboutus', function () {
+    return view('aboutus');
+});
+Route::view('/contact', 'contact', [
+    "name" => "Yash-Tapre"
+]);
+Route::get('/person/{name}', function($name){
+    return "The name is $name.";
+});
+Route::get('/person/{name?}', function(?string $name = 'yashtapre77'){
+    return "The name is $name.";
+})->name('person');
+
